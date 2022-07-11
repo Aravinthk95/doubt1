532702a1bae0e3f0bcd70de4577c7a93acf8e5b2
Aravinthk95 <107410327+Aravinthk95@users.noreply.github.com>
2022-07-11 19:27:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,89 @@
+
+// var request = new XMLHttpRequest();
+
+// request.open('GET','https://restcountries.com/v3.1/all',true);
+
+// request.send();
+
+// request.onload = function(){
+//     var data = JSON.parse(request.response);
+//     console.log(data);
+
+// let asia = data.filter((count) => {
+//     return count.region === "Asia";
+
+// });
+// console.log(asia);
+// }
+
+
+
+// var request = new XMLHttpRequest();
+
+// request.open('GET','https://restcountries.com/v3.1/all',true);
+
+// request.send();
+
+// request.onload = function(){
+//     var data = JSON.parse(request.response);
+//     console.log(data);
+
+// let countries = data.filter((popu) => {
+//     return popu.population < 200000;
+
+// });
+// console.log(countries);
+// }
+
+
+
+// var request = new XMLHttpRequest();
+
+// request.open('GET','https://restcountries.com/v3.1/all',true);
+
+// request.send();
+
+// request.onload = function(){
+//     var data = JSON.parse(request.response);
+//     console.log(data);
+
+// let total = data.filter((elements) => {
+//     return elements.currency === "United States dollar";
+
+// });
+// console.log(total);
+// }
+
+
+
+
+// var request = new XMLHttpRequest();
+
+// request.open('GET','https://restcountries.com/v3.1/all',true);
+
+// request.send();
+
+// request.onload = function(){
+//     var data = JSON.parse(request.response);
+//     console.log(data);
+
+// let tot = data.reduce((acc,curr) => {
+    
+//     return acc+curr;
+// });
+// console.log(tot);
+// }
+
+
+
+
+// var request = new XMLHttpRequest();
+
+// request.open('GET','https://restcountries.com/v3.1/all',true);
+
+// request.send();
+
+// request.onload = function(){
+//     var data = JSON.parse(request.response);
+//     console.log(data);
+
